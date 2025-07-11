1badbe66b37bf570dd8dcf2f669a6cd120fd525d
MeGum <81900588+Anon242@users.noreply.github.com>
2025-07-11 15:52:12 +0300

    Add files via upload

--- inofile/inofile.ino
+++ inofile/inofile.ino
@@ -1,22 +1,115 @@
-#include <R404v5.h>
+#include <QueenUnisense3v1.h>
+#include <iarduino_NeoPixel.h>
 
-#define DEVICE_ID 1
+#define DEVICE_ID 6
 Board queen(DEVICE_ID);
 
-//===================================================================================
-//      Filename  : R404v5
+//=================================================================================== 
+//      Filename  : QueenUnisense3v1
 //      Author    : MeGum
-//      Created   : 20.01.2025 16:17:35
+//      Github    : https://github.com/Anon242/QueenKit
+//      Created   : 2025-07-10 13:45:09
 //      Version   : 1.0
-//      Notes     :
-//                :
-//===================================================================================
+//      Notes     : 
+//                : адреска и analog in
+//=================================================================================== 
 
-void setup() { queen.init(onMessage); }
+class Engine{
+  public:
+  // Принимаю ЦИФРОВЫЕ АРДУИНОВСКИЕ пины, управляю АРДУИНОВСКИМИ МЕТОДАМИ
+  Engine(uint8_t _pinNumGround, uint8_t _pinNumPlus){
+    pinNumGround = _pinNumGround;
+    pinNumPlus =_pinNumPlus;
+  }
+  // Сколько на открытие/закрытие в ms
+  const int timeMs = 3000;
+  // Ардуиновские пины
+  uint8_t pinNumGround , pinNumPlus;
+  bool isOpened = false;
+  bool onMove = false;
 
-void loop() { queen.loop(); }
+  //long ms;
+  //long oldMs;
 
-void onMessage() {
-  queen.out(queen.getBits(0, 7));
-  queen.setBits(0, 5, queen.in());
+  void open(){
+    if(isOpened)
+      return;
+    onMove = true;
+    digitalWrite(pinNumGround,1);
+    digitalWrite(pinNumPlus,0);
+    delay(timeMs);
+    digitalWrite(pinNumGround,0);
+    digitalWrite(pinNumPlus,0);
+
+    isOpened = true;
+    onMove = false;
+  }
+  void close(){
+    if(!isOpened)
+      return;
+    onMove = true;
+    digitalWrite(pinNumGround,0);
+    digitalWrite(pinNumPlus,1);
+    delay(timeMs);
+    digitalWrite(pinNumGround,0);
+    digitalWrite(pinNumPlus,0);
+
+    isOpened = false;
+    onMove = false;
+  }
+};
+
+// !!!!!!!!
+#define LEDCOUNT 20
+iarduino_NeoPixel led(A8, LEDCOUNT); //Инициализируем ленту
+uint8_t colorR = 0;
+uint8_t colorG = 0;
+uint8_t colorB = 0;
+uint8_t colorNum = 0;
+Engine engine = Engine(46,13);
+
+void setup() 
+{ 
+  queen.init(onMessage); 
+  led.begin();
+  delay(300);
+  engine.open();
+}
+
+void loop() 
+{ 
+  queen.loop(); 
+}
+
+void onMessage() 
+{  
+  bitWrite(PORTL,PL4,queen.getBits(0,1));
+  // Получаем буфер ADC
+  uint16_t *adcBuffer = queen.inADC();
+  for (uint8_t i = 0; i < 16; i++) {
+    uint8_t bitIndex = 16 + 10 * i;
+    // Назначаем PWM из шины
+    //queen.pwmOuts(queen.getBits(bitIndex, 10), i + 1);
+    // Отправляем данные ADC в шину
+    queen.setBits(bitIndex, 10, adcBuffer[i]);
+  }
+  // Отправляем цифровые пины в шину
+  queen.setBits(0, 16, queen.in());
+  colorR = queen.getBits(192,10);
+  colorG = queen.getBits(202,10);
+  colorB = queen.getBits(212,10);
+
+  colorNum = queen.getBits(222,10);
+
+  for(int i = 0; i < LEDCOUNT; i++){
+    led.setColor(i, 0, 0, 0); 
+  }
+
+  for(int i = 0; i < colorNum; i++){
+    led.setColor(i, colorR, colorG, colorB); 
+  }
+
+
+
+  led.write();
 }
